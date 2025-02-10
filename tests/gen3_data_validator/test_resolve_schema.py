f8acd4566de49baeeebfecb6d43ba1a50f27eb1c
JoshuaHarris391 <harjo391@gmail.com>
2025-02-10 13:52:11 +1100

    stripped back prototype

---
+++ tests/gen3_data_validator/test_resolve_schema.py
@@ -0,0 +1,15 @@
+# conftest.py
+import pytest
+import json
+from pathlib import Path
+from gen3_data_validator.resolve_schema import ResolveSchema
+
+
+# test_node_link
+
+
+
+# test_find_upstream_downstream
+
+
+
