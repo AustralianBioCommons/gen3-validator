3029565665321d068480a1353d75869917f1ce07
JoshuaHarris391 <harjo391@gmail.com>
2025-07-08 16:43:33 +1000

    added init test

--- tests/gen3_data_validator/test_validate.py
+++ tests/gen3_data_validator/test_validate.py
@@ -5,3 +5,34 @@ from gen3_data_validator.validate import Validate, ValidateStats, ValidateSummar
 from unittest.mock import patch, MagicMock, mock_open
 import os
 
+
+@pytest.fixture
+def mock_data_map():
+    return {
+        'lipidomics_assay': [{'key1': 'value1'}, {'key2': 'value2'}],
+        'sample': [{'key3': 'value3'}, {'key4': 'value4'}]
+    }
+
+
+@pytest.fixture
+def mock_resolved_schema():
+    return {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "object",
+        "properties": {
+            "key1": {"type": "string"},
+            "key2": {"type": "string"},
+        },
+        "additionalProperties": False,
+        "required": []
+    }
+
+
+def test_init_Validate(mock_data_map, mock_resolved_schema):
+    validate = Validate(data_map=mock_data_map, resolved_schema=mock_resolved_schema)
+    assert validate.data_map == mock_data_map
+    assert validate.resolved_schema == mock_resolved_schema
+    assert validate.validation_result is None
+    assert validate.key_map is None
+
+# TODO - Create data folder in tests dir containing schema, resolved schema, and some example data file, maybe two data nodes, with two objects each
