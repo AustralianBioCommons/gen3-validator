8debcc56072632afab0faf4954821a56220cc9b0
JoshuaHarris391 <harjo391@gmail.com>
2025-08-05 22:16:46 +1000

    documenting how to configure for testpypi

--- poetry_refresh_install.sh
+++ poetry_refresh_install.sh
@@ -2,3 +2,12 @@
 rm poetry.lock
 poetry install
 poetry run pytest
+
+
+# # publish to test pypi
+# poetry config repositories.testpypi https://test.pypi.org/legacy/
+# poetry config pypi-token.testpypi <you token>
+# poetry publish -r testpypi --build
+
+
+
